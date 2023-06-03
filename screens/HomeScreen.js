934b29d092e89b6525bdf4e4a98d820023943e7e
hoangthai2310 <133748142+hoangthai2310@users.noreply.github.com>
2023-06-03 23:05:55 +0700

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Button, View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import log from '../Log';
+import Student from '../components/Students';
+
+const HomeScreen = ({ navigation }) => {
+    const [students, setStudents] = useState([]);
+
+    // Hàm điều hướng
+    const navigateToLogin = () => {
+        navigation.navigate('Login');
+    };
+
+    // Gọi function
+    async function getListStudent() {
+        try {
+            const API_URL = 'http://192.168.0.6:3000/students';
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            setStudents(data);
+            log.info('====> students:', JSON.stringify(data));
+            return data;
+        } catch (error) {
+            log.error('Fetch data failed ' + error);
+            return null ;
+        }
+    }
+
+    // Hooks là những hàm cho phép bạn “kết nối” React state và lifecycle vào các components sử dụng hàm.
+    // useState() là 1 react hook
+    // 6 trường hợp sử dụng của useEffect() trong React
+    // 1.Chạy một lần khi mount : tìm nạp data API.
+    // 2.Chạy khi thay đổi state : thường thì khi validate input đầu vào.
+    // 3.Chạy khi thay đổi state : filtering trực tiếp.
+    // 4.Chạy khi thay đổi state : trigger animation trên giá trị của array mới.
+    // 5.Chạy khi props thay đổi : update lại list đã fetched API khi data update.
+    // 6.Chạy khi props thay đổi : updateing data API để cập nhật BTC
+    useEffect(() => {
+        console.log('useEffect has been called!');
+        getListStudent();
+    }, []);
+
+    // Gọi vào hàm return với dữ liệu ban đầu là là danh sách sinh viên rỗng
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.scrollView}>
+                {/* <Button title='Go to Login Screen' onPress={navigateToLogin} /> */}
+                <View>
+                    <Text style={styles.txtHeader}>List Student</Text>
+                </View>
+                <View style={styles.studentContainer}>
+                    {students.map((item, index) => {
+                        return <Student student={item} key={index}></Student>;
+                    })}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    scrollView: {
+        flexGrow: 1,
+        padding: 20
+    },
+    txtHeader: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    studentContainer: {
+        flex: 1
+    }
+});
+
+export default HomeScreen;
---
+++ screens/SignInScreen.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { Alert, Image, StyleSheet, View } from 'react-native';
+import CustomInput from '../components/CustomInput';
+import CustomButton from '../components/CustomButton';
+import log from '../Log';
+
+const SignInScreen = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // Tạo biến users để lưu trữ danh sách user của hệ thống
+    let users = [];
+
+    // Function lấy dữ liệu từ API sử dụng fetch
+    async function fetchData() {
+        try {
+            // Khai báo đường dẫn API
+            const API_URL = 'http://192.168.0.6:3000/users';
+            const response = await fetch(API_URL);
+            // .json() chuyển đổi data trả về từ API sang json
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            log.error('Fetch data failed ' + error);
+            return null;
+        }
+    }
+
+    // Function gọi fetchData sau đó lưu response từ API trả về vào biến users
+    async function storeData() {
+        users = await fetchData();
+        log.info('users: ' + JSON.stringify(users));
+    }
+
+    storeData();
+
+    // Funtion thực hiện đăng nhập
+    const doLogin = () => {
+        // Kiểm tra dữ liệu trên form gồm username và password
+        if (username.length == 0) {
+            Alert.alert('Username is required');
+            return;
+        }
+
+        if (password.length == 0) {
+            Alert.alert('Password is required');
+            return;
+        }
+
+        // Tạo đối tượng lưu giữ thông tin login
+        let request = { username: username, password: password };
+
+        // In ra thông tin user phục vụ check lỗi
+        log.info('authInfo: ' + JSON.stringify(request));
+
+        // Kiêm tra danh sách users có null hoặc undefined không
+        if (users) {
+            const authInfo = users.find((user) => user.userName === request.username);
+            // Thực hiện validate dữ liệu trên form và hiển thị alert
+            if (!authInfo) {
+                Alert.alert('Notification', 'Cant find user infomation', [{ text: 'Cancel', onPress: () => log.error('Cant find user ' + request.username) }]);
+            } else {
+                if (!(authInfo.password === request.password)) {
+                    Alert.alert('Notification', 'Password is not correct', [{ text: 'Cancel', onPress: () => log.error('Password is not correct for ' + request.username) }]);
+                } else {
+                    Alert.alert('Notification', 'Login successfull ' + request.username, [
+                        { text: 'OK', onPress: () => navigateToHome() },
+                        { text: 'Cancel', onPress: () => log.info('Press Cancel') }
+                    ]);
+                }
+            }
+        }
+    };
+
+    const navigateToHome = () => {
+        props.navigation.navigate('Home');
+    };
+
+    return (
+        <View style={styles.root}>
+            <Image source={require('../assets/logo.png')} style={styles.logo} />
+            <CustomInput placeholder='Username' value={username} setValue={setUsername} secureTextEntry={false} />
+            <CustomInput placeholder='Password' value={password} setValue={setPassword} secureTextEntry={true} />
+            <CustomButton btnLabel={'Sign In'} onPress={doLogin} />
+            <CustomButton btnLabel={'Back to Home'} onPress={navigateToHome} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    root: {
+        alignItems: 'center',
+        padding: 20
+    },
+    logo: {
+        width: '50%',
+        height: '50%',
+        resizeMode: 'contain'
+    }
+});
+
+export default SignInScreen;
